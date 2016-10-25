a1bd9bd57784f377303eef7f28ff335fe636c06c
Chad Coleman <cjc2238@tc.columbia.edu>
2016-10-25 10:16:59 -0400

    Started creating resume function for shiny app.

---
+++ My_Resume_Function.R
@@ -0,0 +1,6 @@
+
+Visual.Resume <- function(left.titles = c("My Name", "My Job", "SubTitle"),
+                          right.titles = c("Website", "Email"),
+                          year.range = c(2008, 2016), 
+                          graph.labels = c("Academics", "Work"), 
+                          data = rbind(c("Tallahassee Community College", "A.A.", "2008", "2010", 65, "top", 2009, "right",70),
---
+++ Scripted Resume.R
@@ -0,0 +1,16 @@
+##################
+## Load Library ##
+##################
+
+library(ggplot2)
+
+#########################
+## Create Data Frame ####
+#########################
+
+months = c("5","18","39","9","26")
+job = c("Educational Research Analyst","Hybrid Course Instructional Designer","Systems Administrator","Support Analyst","Network Technician")
+df <- data.frame(job, months)
+
+
+
